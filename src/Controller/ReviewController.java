ef1cdef6478b7eda029c6f83ca6009bbbaa89766
OngTK <dhdxorud@gmail.com>
2025-08-03 21:11:31 +0900

    Review 관련 DTO 정리

--- src/Controller/ReviewController.java
+++ src/Controller/ReviewController.java
@@ -15,25 +15,25 @@ public class ReviewController {
 
     // 메소드 =====================================================================================
 
-    // [RV-00] /
+    // [review00] /
     // 매개변수 :
     // 반환타입 :
     // 반환 :
     // [0.1]
 
-    // [RV-01] 리뷰전체조회 / reviewPrint()
+    // [review01] 리뷰전체조회 / reviewPrint()
     // 매개변수 : -
     // 반환타입 : ArrayList<reviewPrintDto>
     // 반환 : ArrayList<reviewPrintDto> 전체 출력
     // [1.1]
 
-    // [RV-02] 가맹점리뷰조회 / franReviewPrint()
+    // [review02] 가맹점리뷰조회 / franReviewPrint()
     // 매개변수 : String franName
     // 반환타입 : ArrayList<reviewPrintDto>
     // 반환 : ArrayList<reviewPrintDto> 출력
     // [2.1]
 
-    // [RV-03] 상품별리뷰 / proReviewPrint()
+    // [review03] 상품별리뷰 / proReviewPrint()
     // 매개변수 : String proName
     // 반환타입 : ArrayList<reviewPrintDto>
     // 반환 : ArrayList<reviewPrintDto> 출력
--- src/Model/DAO/IOLogDao.java
+++ src/Model/DAO/IOLogDao.java
@@ -34,37 +34,37 @@ public class IOLogDao {
 
     // 메소드 =====================================================================================
 
-    // [IO-00]
+    // [IOLog00]
     // 매개변수 :
     // 반환타입 :
     // 반환 :
 
-    // [IO-01] 재고로그등록·재고등록 /  ioLogAdd()
+    // [IOLog01] 재고로그등록·재고등록 /  ioLogAdd()
     // 매개변수 : String proName, int ioQty
     // 반환타입 : boolean
     // 반환 : true 성공 / false 실패
     // [1.1]
     // [1.2]
 
-    // [IO-02] 재고로그조회 / IOLogPrint()
+    // [IOLog02] 재고로그조회 / IOLogPrint()
     // 매개변수 : -
     // 반환타입 : ArrayList<IOLogDto>
     // 반환 : ArrayList<IOLogDto> 출력
     // [2.1]
 
-    // [IO-03] 단일재고로그조회 / oneIOLogPrint()
+    // [IOLog03] 단일재고로그조회 / oneIOLogPrint()
     // 매개변수 : int ioNo
     // 반환타입 : IOLogDto
     // 반환 : IOLogDto 출력
     // [3.1]
 
-    // [IO-04] 재고조회 / IOPrint()
+    // [IOLog04] 재고조회 / IOPrint()
     // 매개변수 : -
     // 반환타입 : ArrayList<IOLogDto>
     // 반환 : ArrayList<IOLogDto> 출력, 단, 상품번호별 합산 후 출력
     // [4.1]
 
-    // [IO-05] 재고로그수정 / ioUpdate()
+    // [IOLog05] 재고로그수정 / ioUpdate()
     // 매개변수 : int ioNo, int proNo, String IO, int ioQty, String ioMemo
     // 반환타입 : boolean
     // 반환 : true 성공 / false 실패
